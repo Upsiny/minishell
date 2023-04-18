4ab8759bf50b53dd37b63b1be3b787f401bafdf0
Paufert Thomas <Thomas@MacBook-Pro-de-Paufert.local>
2023-04-18 14:55:53 +0200

    avec vendor

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,15 +6,16 @@
 /*   By: tpaufert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 11:47:27 by hguillau          #+#    #+#             */
-/*   Updated: 2023/04/14 15:52:48 by tpaufert         ###   ########.fr       */
+/*   Updated: 2023/04/17 18:35:12 by tpaufert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include <readline/readline.h> 
-# include <stdlib.h>
 
 #endif
